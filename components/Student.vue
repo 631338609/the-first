c2300bbbb13da9e307abcc7dd1d7371f49954e99
小巨人申多多 <87165637+631338609@users.noreply.github.com>
2021-09-25 15:06:44 +0800

    Add files via upload

---
+++ components/Student.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <h2>学生姓名：{{name}}</h2>
+        <h2>学生性别：{{sex}}</h2>
+        <h2>学生年龄：{{myAge}}</h2>
+        <button @click="updateAge">加年龄</button>
+    </div>
+</template>
+<script>
+export default {
+    name:'Student',
+    data() {
+        return {
+            // 传进的值进行一个保存，就可以动态改变值在组件中显示
+            myAge:this.age
+            // name:"申多多",
+            // age:18,
+            // sex:'女'
+
+        }
+    },
+    methods:{
+        updateAge(){
+            this.myAge++;
+        }
+    },
+    // props:['name','sex','age']
+    props:{
+        name:{
+            // 对类型的限制
+            type:String,
+            // 默认值的限制
+            default:'啦啦啦',
+            // 必须存在的值，与defaul不会同时存在
+            require:true
+            },
+            
+        //对类型限制的简便写法
+        age:Number,
+        sex:String
+    }
+}
+</script>
--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-// 该文件是整个项目的入口文件
-// 引入vue
-import Vue from 'vue'
-// 引入app组件，他是所有组件的父组件
-import App from './App.vue'
+// 引入Vue
+import Vue from "vue";
+// 引入App
+import App from "./App.vue"
 // 关闭vue的生产提示
-Vue.config.productionTip = false
-// 创建vue实例对象 ----vm
+Vue.config.productionTip=false
 new Vue({
-  // 将app组件放入容器中
-  el:'#app',
-  render: h => h(App),
+    el:"#app",
+    render:h => h(App)
 })
