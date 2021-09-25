a70c301bf9c145ae7250d5fccb29307d544ef0b3
小巨人申多多 <87165637+631338609@users.noreply.github.com>
2021-09-25 15:10:36 +0800

    Add files via upload

---
+++ 01_src_分析脚手架/main.js
@@ -0,0 +1,13 @@
+// 该文件是整个项目的入口文件
+// 引入vue
+import Vue from 'vue'
+// 引入app组件，他是所有组件的父组件
+import App from './App.vue'
+// 关闭vue的生产提示
+Vue.config.productionTip = false
+// 创建vue实例对象 ----vm
+new Vue({
+  // 将app组件放入容器中
+  el:'#app',
+  render: h => h(App),
+})
