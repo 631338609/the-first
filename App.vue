bc4a28b49f1f4a4dba9b068fb421e6873d5efcd3
小巨人申多多 <87165637+631338609@users.noreply.github.com>
2021-09-25 14:56:04 +0800

    Add files via upload

--- App.vue
+++ App.vue
@@ -1,14 +1,28 @@
 <template>
-    <div>
-        <Student name="李四" sex="女" :age="19"></Student>
-    </div>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
-<script>
-import Student from './components/Student'
-export default {
-    name:'App',
-    components:{Student},
-    
 
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
 }
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-// 引入Vue
-import Vue from "vue";
-// 引入App
-import App from "./App.vue"
+// 该文件是整个项目的入口文件
+// 引入vue
+import Vue from 'vue'
+// 引入app组件，他是所有组件的父组件
+import App from './App.vue'
 // 关闭vue的生产提示
-Vue.config.productionTip=false
+Vue.config.productionTip = false
+// 创建vue实例对象 ----vm
 new Vue({
-    el:"#app",
-    render:h => h(App)
+  // 将app组件放入容器中
+  el:'#app',
+  render: h => h(App),
 })
